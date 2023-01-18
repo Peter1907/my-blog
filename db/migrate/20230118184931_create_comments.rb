c14378d38bb14cb3431434dc4ec707329dcea725
Peter1907 <peter.nady.1408@gmail.com>
2023-01-18 20:55:28 +0200

    Generate the schema file

--- db/migrate/20230118184931_create_comments.rb
+++ db/migrate/20230118184931_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
     create_table :comments do |t|
       t.text :text
       t.references :author_id, null: false, foreign_key: { to_table: :users }
-      t.references :post_id, null: false, foreign_key: true
+      t.references :post_id, null: false, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
