144520167b8a5eea83267d5756cf77059cc3e566
Peter1907 <peter.nady.1408@gmail.com>
2023-01-19 12:41:57 +0200

    Add the post model

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
+
+  scope :last_5, -> { order(created_at: :desc).limit(5) }
+  
+  after_create :update_post_counter
+  after_destroy :update_post_counter
+
+  private
+  def update_post_counter
+    self.author.posts_counter = self.author.posts.count
+  end
+end
