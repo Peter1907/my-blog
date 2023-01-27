94a9e3bea4860b67aba16b71f92e956a47bd8023
Peter1907 <peter.nady.1408@gmail.com>
2023-01-27 22:00:16 +0200

    Add new comment form & Update the controller's actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,27 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+    @comment.post = Post.find(params[:post_id])
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          flash[:notice] = 'Comment was successfully created.'
+          redirect_to user_post_path(current_user, params[:post_id])
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
+  validates :text, presence: true, length: { in: 5..1000 }
+
   after_create :update_comment_counter
   after_destroy :update_comment_counter
 
