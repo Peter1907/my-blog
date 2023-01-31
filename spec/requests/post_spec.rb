de5f1d9f52284a3cd3425ee870e9ba3df1744e81
Peter1907 <peter.nady.1408@gmail.com>
2023-01-31 22:44:57 +0200

    Add database-cleaner & Fix test errors

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :request do
   describe 'Index action' do
-    before(:each) { get user_posts_path(1) }
+    before(:each) do
+      @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      get user_posts_path(@user)
+    end
 
     it 'has the correct response status' do
       expect(response).to have_http_status(200)
@@ -13,23 +16,27 @@ RSpec.describe User, type: :request do
     end
 
     it 'renders the view with the correct placeholder text' do
-      expect(response.body).to include('list of all the posts for a specific user')
+      expect(response.body).to include('Number of posts: 0')
     end
   end
 
   describe 'Show action' do
-    before(:each) { get user_post_path(1, 1) }
+    before(:each) do
+      @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      @post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
+      get user_post_path(@user, @post)
+    end
 
     it 'has the correct response status' do
       expect(response).to have_http_status(200)
     end
 
     it 'renders the correct template' do
-      expect(response).to render_template('show')
+      expect(response).to render_template(:show)
     end
 
     it 'renders the view with the correct placeholder text' do
-      expect(response.body).to include('details about a specific post of a certain user')
+      expect(response.body).to include('This is my first post')
     end
   end
 end
