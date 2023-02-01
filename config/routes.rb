579165e5063fed8aac499738bc04689842e6e176
Peter1907 <peter.nady.1408@gmail.com>
2023-02-02 00:01:34 +0200

    Add mailcatcher config to confirm email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'confirmations',
+    sessions: 'sessions',
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
