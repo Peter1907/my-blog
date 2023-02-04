e0a7e23022dfe5dfcd30fbca7f4aa76a23da9da4
Peter1907 <peter.nady.1408@gmail.com>
2023-02-04 02:31:12 +0200

    Create routes for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index show] do
+        resources :posts, only: %i[index show new create destroy] do
+          resources :comments, only: %i[new create destroy]
+        end
+      end
+    end
+  end
+
   devise_for :users, controllers: { confirmations: 'confirmations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
